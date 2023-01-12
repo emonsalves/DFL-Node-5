eb8a1f2a5260f470e85c4dd935c28cd919002a5d
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-01-12 20:31:04 -0300

    Feat : EndPoint GetAll with HETEOAS

--- src/models/joyasModels.js
+++ src/models/joyasModels.js
@@ -1,8 +1,11 @@
 const pool = require("../helpers/connectionDb").getInstance();
+const format = require('pg-format')
 
-const getJoyas = async () => {
+const getJoyas = async ({ limits = 3, order_by = "stock_ASC", page = 1 }) => {
+    const [campo, direccion] = order_by.split("_")
+    const offset = (page - 1) * limits
+    const SQLquery = format('SELECT * FROM inventario order by %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset)
 
-    SQLquery = { text: "SELECT * FROM inventario" }
     try {
         const result = await pool.query(SQLquery)
         return result.rows
@@ -11,6 +14,4 @@ const getJoyas = async () => {
     }
 }
 
-module.exports = {
-    getJoyas
-}
+module.exports = { getJoyas }
