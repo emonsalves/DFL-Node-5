cf1e3f46cbe75d4f58e0e7463d402508554f00a8
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-01-13 11:14:39 -0300

    Fix : middleware

--- src/middlewares/controlQuery.js
+++ src/middlewares/controlQuery.js
@@ -1,11 +1,16 @@
 const controlQuery = async (req, res, next) => {
-    const parametros = req.query
-    const url = req.url
+    const params = req.params;
+    const query = req.query;
+    const body = req.body;
+    const url = req.url;
     console.log(`
 	Hoy ${new Date()}
 	Se ha recibido una consulta en la ruta ${url}
-	con los parámetros:
-	`, parametros)
+	con los siguientes datos: `,
+        "por params", params,
+        "por query", query,
+        "por body", body
+    );
     next()
 }
 
