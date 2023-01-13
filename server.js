24f0aaf38aa93f5d5d54535207fae886c1967357
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-01-13 08:12:05 -0300

    Fix : Middleware position

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const morgan = require('morgan');
 const morganBody = require('morgan-body')
 const CsbInspector = require('csb-inspector');
+const {controlQuery} = require('./src/middlewares/controlQuery')
+
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(controlQuery)
 morganBody(app);
 CsbInspector();
 
